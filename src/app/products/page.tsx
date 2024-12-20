429fc131aefb3eb44a6c30c96e693a617e65d11d
Alan Oliveira <alanprot7@gmail.com>
2024-12-20 09:54:01 -0300

    Utilizando link e replace aula 09

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,15 +1,19 @@
 import { Metadata } from "next"
+import Link from "next/link"
 
 export const metadata: Metadata = {
     title: 'Produtos'
 }
 
 export default function Products(){
+
+    const id = 1
+
     return (
         <>
-            <h1>Produto 1</h1>
-            <h1>Produto 2</h1>
-            <h1>Produto 3</h1>
+            <Link href={`/products/${id}`}>Produto 1</Link><br />
+            <Link href={'/products/2'}>Produto 2</Link><br />
+            <Link href={'/products/3'} replace>Produto 3</Link>
         </>
     )
 }
