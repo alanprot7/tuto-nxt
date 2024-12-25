c00bc1a72aa5fe8694635e263062dd694a5f4bd7
Alan Oliveira <alanprot7@gmail.com>
2024-12-24 21:46:56 -0300

    aulas ate 19, crud de rotas dinamicas

--- src/app/names/[id]/route.ts
+++ src/app/names/[id]/route.ts
@@ -1,7 +1,7 @@
 import { data } from "../data";
 
 export async function GET(
-    _request,
+    _request: Request,
     {params} : { params: {id: string}}
 ){
     const name = data.find(name => {
@@ -9,4 +9,33 @@ export async function GET(
     })
 
     return Response.json(name)
+}
+
+
+export async function PATCH(
+    request: Request,
+    { params }: { params : { id: string } }
+){
+    const body = await request.json()
+    
+    const { text } = body
+    const index = data.findIndex(user => {
+        return user.id === parseInt(params.id)
+    })
+
+    data[index].name = text
+    return Response.json(data[index])
+}
+
+export async function DELETE(
+    request: Request,
+    { params }: { params : { id: string } }
+){
+    const index = data.findIndex(user => {
+        return user.id === parseInt(params.id)
+    })
+
+    const deletedUser = data[index]
+    data.splice(index, 1)
+    return Response.json(deletedUser)
 }
