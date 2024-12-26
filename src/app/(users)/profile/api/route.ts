b2cc5178f4aeca7de33d24256bc8aff221c9314d
Alan Oliveira <alanprot7@gmail.com>
2024-12-26 14:06:51 -0300

    Trabalhando com headers e tb no response

--- src/app/(users)/profile/api/route.ts
+++ src/app/(users)/profile/api/route.ts
@@ -1,3 +1,20 @@
-export async function GET() {
-    return new Response('Profile API')
+import { NextRequest } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(request: NextRequest) {
+
+    const requestHeaders = new Headers(request.headers)
+    const headerList = headers()
+
+    //2x formas de pegar os headers de uma requisição http
+
+    console.log(requestHeaders.get('Authorization'))
+    console.log(headerList.get('Authorization'))
+
+    // Retornando response com headers
+    return new Response('<h1>Profile API</h1>', {
+        headers: {
+            "Content-Type": "text/html"
+        }
+    })
 }
